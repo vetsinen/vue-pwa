22a18ccd2eba8abc093f321e3fe3cbec8a3de797
vetsinen <vetsinen@gmail.com>
2019-03-31 15:14:52 +0300

    deploy

---
+++ precache-manifest.b51d1a7ddde33b8caeca262b5a6eea20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6989fcf932b8224396fb",
+    "url": "/vue-pwa/css/app.a1692e97.css"
+  },
+  {
+    "revision": "6989fcf932b8224396fb",
+    "url": "/vue-pwa/js/app.70a6615b.js"
+  },
+  {
+    "revision": "0636858c332e644883fa",
+    "url": "/vue-pwa/js/chunk-vendors.9b96e0fb.js"
+  },
+  {
+    "revision": "ee5cd8b02fb388d83cffbea0948a305e",
+    "url": "/vue-pwa/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa/robots.txt"
+  }
+];
